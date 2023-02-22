0a8aded6f5f675b02e2b716feaefd1f4be1363c7
Zi heng Liu <84878485+ZihengL@users.noreply.github.com>
2023-02-22 13:54:18 -0500

    Modifs Calc class

---
+++ app/src/test/java/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.function.BinaryOperator;
+
+public class Calculator {
+
+    public static double a;
+    public static double b;
+
+    public static double operate(double a, double b, String symbol) {
+
+
+        return 0.;
+    }
+}
